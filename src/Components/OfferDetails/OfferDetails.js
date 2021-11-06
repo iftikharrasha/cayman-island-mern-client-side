f75c43289fc9a5e093dc3ef942449b5367ed96de
intisar eusha <iftikharrasha@gmail.com>
2021-11-07 04:26:06 +0600

    single details of offer fetched

--- src/Components/OfferDetails/OfferDetails.js
+++ src/Components/OfferDetails/OfferDetails.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { offers } from '../../FakeData/offers';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const OfferDetails = (props) => {
     const { orderId } = useParams();
-    const orderDetail = offers.find(offer => offer._id == orderId);
-    const {_id, category, title, img, key, price, desc} = orderDetail;
+
+    const [offer, setOffer] = useState([]);
+    useEffect(() => {
+        const url = `http://localhost:5000/place-order/${orderId}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setOffer(data));
+    }, [])
 
     return (
         <>
@@ -22,14 +28,14 @@ const OfferDetails = (props) => {
                     <div className="hero--top page--top">
                         <Row className="overflowX">
                             <Col lg={12} className="order-lg-1 order-2 pt-lg-0 pt-5 text-lg-left text-center mb-lg-5 mb-0 " data-aos="fade-left" data-aos-duration="1000" data-aos-once="true">
-                                <img className="img-fluid banner" src={img} alt={key}/>
+                                <img className="img-fluid banner" src={offer.img} alt={offer.key}/>
                             </Col>
                             <Col lg={12} className="order-lg-2 order-1" data-aos="fade-right" data-aos-duration="1000" data-aos-once="true">
-                                <h1 className="bold--30">{title}</h1>
-                                <h2 className="my-4 bold--30">Category: {category}</h2>
-                                <h4 className="lit--16">{desc}</h4>
+                                <h1 className="bold--30">{offer.title}</h1>
+                                <h2 className="my-4 bold--30">Category: {offer.category}</h2>
+                                <h4 className="lit--16">{offer.desc}</h4>
                                 <p className="mt-4 bold--16">Price Starting from:</p> 
-                                <h5 className="lit--16">{price}$/Night</h5>
+                                <h5 className="lit--16">{offer.price}$/Night</h5>
                             </Col>
                         </Row>
                     </div>
